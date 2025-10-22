8980d7ac638d1a3c0595671fb3743f784fd9a621
dreule <danielreule28@gmail.com>
2025-10-22 12:27:48 +0200

    FIX(MateriaSource.cpp): fixed leaks

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -1,13 +1,19 @@
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource() {
+MateriaSource::MateriaSource()
+{
+	for (int i = 0; i < 4; ++i)
+		learned[i] = NULL;
 	std::cout << "MateriaSource ctor called" << std::endl;
 }
 
 MateriaSource::MateriaSource(const MateriaSource& other)
 {
 	for (int i = 0; i < 4; ++i)
-		this->learned[i] = other.learned[i];
+		if (learned[i])
+			this->learned[i] = other.learned[i]->clone();
+		else
+			this->learned[i] = NULL;
 	std::cout << "MateriaSource copy-ctor called" << std::endl;
 }
 
@@ -16,12 +22,25 @@ MateriaSource&	MateriaSource::operator=(const MateriaSource& other)
 	if (this != &other)
 	{
 		for (int i = 0; i < 4; ++i)
-			this->learned[4] = other.learned[i];
+		{
+			if (learned[i])
+			{
+				delete learned[i];
+				learned[i] = NULL;
+			}
+		}
+		for (int i = 0; i < 4; ++i)
+			if (learned[i])
+				this->learned[i] = other.learned[i]->clone();
 		std::cout << "MateriaSource assignment operator called" << std::endl;
 	}
 	return (*this);
 }
 
-MateriaSource::~MateriaSource() {
+MateriaSource::~MateriaSource()
+{
+	for (int i = 0; i < 4; ++i)
+		if (learned[i])
+			delete learned[i];
 	std::cout << "MateriaSource dtor called" << std::endl;
 }
